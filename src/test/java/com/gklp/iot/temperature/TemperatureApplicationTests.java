2c7c532be8b16da81dd20a1aa32581a0b81e9b79
gklp <gokalpkuscu@gmail.com>
2023-04-16 13:58:55 +0300

    initial commit

---
+++ src/test/java/com/gklp/iot/temperature/TemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gklp.iot.temperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
